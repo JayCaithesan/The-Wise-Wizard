26c5bff21459b4b3a8321ae376dce42b7d66bbe7
JayCaithesan <jaycaithesan@gmail.com>
2023-03-13 08:42:41 -0400

    Added more documentation

--- scripts/wizard.js
+++ scripts/wizard.js
@@ -1,31 +1,20 @@
 
-    /*
-    //read the json file
-    const fs = require("fs");
-    //uses the fs library in node.js to read
-    let json = fs.readFileSync('data/advice.json');
-    //parses the json files from json text to js objects
-    let parsedJson = JSON.parse(json);
 
-    console.log(parsedJson.quotes[0].quote);
-    */
+let quotes = []
 
-    
-    let quotes = []
-    fetch("data/advice.json")
-                .then((resp) => resp.json())
-                .then(function (data) {
-                    // success
-                    quotes = data;
-                    //let parsed = JSON.parse(quotes)
-                    //changeQuote(quotes[0].quote)
+//fetchs the data from the json file
+fetch("data/advice.json")
+            .then((resp) => resp.json())
+            .then(function (data) {
+                  
+                quotes = data;
                     
                     
-                })
-                .catch(function (error) {
-                    // error
-                    console.log(error);
-                });
+            })
+            .catch(function (error) {
+                // catch the error if fetch fails
+                console.log(error);
+            });
                 
 
 
@@ -35,7 +24,11 @@ function changeQuote()
 
     //changes the quote
     var quotePlace = document.getElementById('quotePlace');
+
+    //gets a random number/element of the array to display a quote
     const num = Math.floor(Math.random()*quotes.length)
     var quote = quotes[num].quote;
+
+    //sets the html of 'quoteplace' to the quote
     quotePlace.innerHTML = quote;
 }
